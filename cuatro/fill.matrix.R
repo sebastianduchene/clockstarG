567cdfa691393be785cc13847e999b784ab0cad5
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-03-30 15:48:31 +1100

    Completed task 4.1 and 4.2 	modified:   README.md

--- cuatro/fill.matrix.R
+++ cuatro/fill.matrix.R
@@ -1,32 +1,27 @@
-matrix.name <- "sbsdtest2.txt"
 
 
 
-dat.mat <- read.table(matrix.name, head = F, row.names = 1)
+#matrix.name <- "sbsdmindat.txt"
 
-#dat.mat <- rbind(rep(NA, ncol(dat.mat)), dat.mat)
+fill.matrix <- function(matrix.name, new.matrix.name = NULL){
 
-dat.mat <- cbind(dat.mat , V19=rep(NA, nrow(dat.mat)))
+	    if(is.null(new.matrix.name)) new.matrix.name <- matrix.name
 
-
-for(i in 1:nrow(dat.mat)){
-dat.mat[i, i:ncol(dat.mat)] <- dat.mat[i:ncol(dat.mat), i]
-}
+	    dat.mat <- read.table(matrix.name, head = F, row.names = 1)
 
 
+	    dat.mat <- cbind(dat.mat , VX=rep(NA, nrow(dat.mat)))
 
+	    dat.mat <- as.matrix(dat.mat)
 
-#diag.dim <- length(diag(as.matrix(dat.mat)))
+	    for(i in 1:nrow(dat.mat)){
+	      dat.mat[i, i:ncol(dat.mat)] <- dat.mat[i:ncol(dat.mat), i]
+	   }
 
+	   diag(dat.mat) <- 0
+	   colnames(dat.mat) <- rownames(dat.mat)
 
+	   write.table(dat.mat, file = new.matrix.name, append = F, row.names = T, col.names = T)
+} 
 
-
-dat.mat <- as.matrix(dat.mat)
-
-diag(dat.mat) <- 0
-
-
-
-#for(k in 1:diag.dim){
-#dat.mat[1:diag.dim, 1:diag.dim] <- 0
-#}
+#fill.matrix("sbsdmindat.txt", new.matrix.name = "fullmat.txt")
