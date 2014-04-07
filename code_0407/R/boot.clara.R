e6c6b1e99fa17a1efab8ad3bb9d088ff52a11c51
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-04-07 17:54:28 +1000

    added optional name for boot.clara for paralellization

--- code_0407/R/boot.clara.R
+++ code_0407/R/boot.clara.R
@@ -1,14 +1,15 @@
 
 
-boot.clara <- function(clus.matrix.name, out.boot.name = "out_boot.txt", nboot = 10, k.range = NULL){
+boot.clara <- function(clus.matrix.name, out.boot.name = "out_boot.txt", nboot = 10, k.range = NULL, boot.temp.name = "boot.temp.txt"){
   dat <- read.table(clus.matrix.name, head = T, row.name = 1)
   dat.boot <- dat
   for(k in 1:nboot){
     for(i in 1:ncol(dat.boot)){
       dat.boot[,i] <- runif(nrow(dat), min(dat[, i]), max(dat[, i])) 
     }
-    write.table(dat.boot, "boot.temp.txt")
-    run.clara(clus.matrix.name = "boot.temp.txt", out.clus.name = out.boot.name, k.range = k.range)  
+    write.table(dat.boot, paste0(boot.temp.name, k))
+    run.clara(clus.matrix.name = paste0(boot.temp.name, k), out.clus.name = out.boot.name, k.range = k.range)
+    system(paste0("rm ", boot.temp.name, k))  
   }
 }
 
