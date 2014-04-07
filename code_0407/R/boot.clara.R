9f48a10354f2806beb5d41789e520cfa067c59cf
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-04-07 13:47:25 +1000

    Model testing available on optim.trees.g, new function to bootstrap for run.clara, and 3d plots

---
+++ code_0407/R/boot.clara.R
@@ -0,0 +1,14 @@
+
+
+boot.clara <- function(clus.matrix.name, out.boot.name = "out_boot.txt", nboot = 10){
+  dat <- read.table(clus.matrix.name, head = T, row.name = 1)
+  dat.boot <- dat
+  for(k in 1:nboot){
+    for(i in 1:ncol(dat.boot)){
+      dat.boot[,i] <- runif(nrow(dat), min(dat[, i]), max(dat[, i])) 
+    }
+    write.table(dat.boot, "boot.temp.txt")
+    run.clara(clus.matrix.name = "boot.temp.txt", out.clus.name = out.boot.name)  
+  }
+}
+
--- code_0407/R/optim.trees.g.R
+++ code_0407/R/optim.trees.g.R
@@ -1,5 +1,5 @@
 optim.trees.g <-
-function(data.folder, init.alin = NULL, end.alin = NULL, out.trees = "out.trees"){
+function(data.folder, init.alin = NULL, end.alin = NULL, out.trees = "out.trees", model.test = F, out.models = "out.models.txt"){
 
   require(ape)
   require(phangorn)
@@ -25,9 +25,24 @@ function(data.folder, init.alin = NULL, end.alin = NULL, out.trees = "out.trees"
 
   for(i in range.alin){
       print(paste("OPTIMISING FOR", alin.names[i]))
-      dat.temp <- read.phyDat(alin.names[i], format = "fasta")
+      dat.temp <- tryCatch(read.phyDat(alin.names[i], format = "fasta"), error = function(x) "file not read")
+      if(class(dat.temp) == "character"){
+        print(paste("file", alin.names[i], "not read"))
+	cat(alin.names[i], file = "errors.log", sep = "\n", append = T)
+        next
+      }
       pml.temp <- pml(tree = fix.tree, data = dat.temp)
-      optim.temp <- optim.pml(pml.temp)
+
+      if(model.test){
+        print(paste("Tesing models for", alin.names[i]))
+        model.temp <- modelTest(pml.temp, model = c("GTR", "HKY"))
+        model.temp <- model.temp[order(model.temp$BIC), ][1, 1]	
+	print(paste("The model for", alin.names[i], "is", model.temp))
+	cat(paste(alin.names[i], model.temp, collapse = " "), file = "models.test.txt", sep = "\n", append = T)
+	optim.temp <- optim.pml(pml.temp, model = c("GTR", "HKY")[1+as.numeric(grepl("HKY", model.temp))], optInv = grepl("[+]I", model.temp), optGamma = grepl("[+]G", model.temp))
+      }else{
+	optim.temp <- optim.pml(pml.temp)
+      }
       write.tree(phy = optim.temp$tree, file  = out.trees, append  = T, tree.names = alin.names[i])
       rm(list = c("dat.temp", "pml.temp", "optim.temp"))
   }
