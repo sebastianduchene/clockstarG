9a8a90e2bf2b7e95e1f9927a3ff865579c8c7071
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-04-10 20:53:49 +1000

    completed get.gap

--- code_0407/R/get.gap.R
+++ code_0407/R/get.gap.R
@@ -2,20 +2,16 @@ get.gap <- function(true.data, boot.data){
 
    boot.means <- sapply(true.data[,1], function(x) mean(boot.data[boot.data[,1] == x, 2]))
 
-
-
    gap.dat <- boot.means - true.data[, 2]
    gap.se <- vector()
+   B <- table(boot.data[, 1])[1]
    for(i in true.data[, 1]){
       boot.temp <- boot.data[boot.data[, 1] == i, 2]
       gap.temp <- boot.temp - true.data[true.data[, 1] == i, 2]
       
-
       #gap.se <- c(gap.se, diff(quantile(gap.temp, c(0.05, 0.95))))
 
-# Get number of bootstrap replicates from the data and replace in gap.se
-
-      gap.se <- c(gap.se, sqrt((1 - 1/500)*var(gap.temp)))
+      gap.se <- c(gap.se, sqrt((1 - 1/B)*var(gap.temp)))
    }
    gap.se <- gap.se / true.data[, 1]
    res.mat <- cbind(gap.dat, gap.se)
