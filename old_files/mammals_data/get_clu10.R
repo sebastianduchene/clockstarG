2a46dfd1b441d487449a5f5510f09fcba22ca65e
sebastianduchene <sebastian.duchene@sydney.edu.au>
2014-07-31 23:23:50 +1000

    added

--- old_files/mammals_data/get_clu10.R
+++
@@ -1,7 +0,0 @@
-clu_10 <- read.table('cluster_10.txt', head = T, as.is = T)
-
-class_dat <- read.table('classification_data.csv', sep = ',', head = T, as.is = T)
-
-for(i in 1:nrow(clu_10)){
-      class_dat[class_dat[, 1] == clu_10[i, 1], 7] <- clu_10[i, 2]
-}
--- old_files/mammals_data/upm_clock_test/concat_data.R
+++
@@ -1,7 +0,0 @@
-#dat <- read.dna("all_concat_2.phy")
-
-for(i in 1:nrow(dat)){
-      print(paste("pasting", i))
-      print(length(dat[i, ]))
-      cat(paste(rownames(dat)[i],  toupper(paste(dat[i, ], collapse = "")), sep = "   "), file = "allconc_5.phy", sep = "\n", append = T)
-}
